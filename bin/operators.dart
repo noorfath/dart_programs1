f67b9bc9c79bdc416d9b5bff76cac7fbc02888b8
noorfath <fathimanoora5656@gmail.com>
2023-05-17 12:35:21 +0530

    commit

---
+++ bin/operators.dart
@@ -0,0 +1,19 @@
+void main(){
+  dynamic a=500,b=40;
+  // var result=a/b;
+  print("a+b =${a+b}");
+  print("a-b =${a-b}");
+  print("a/b =${a/b}");
+  print("a*b =${a*b}");
+  print("a%b =${a%b}");
+  print("a~/b =${a~/b}");
+print("-----------------------");
+print("a=b ->${a=b}");
+print("a+=b->${a+=b}");
+print("a-=b->${a-=b}");
+print("a/=b->${a/=b}");
+print("a*=b->${a*=b}");
+print("a%=b->${a%=b}");
+  print("a~/=b->${a~/=b}");
+
+}
