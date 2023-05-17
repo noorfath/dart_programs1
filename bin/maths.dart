7a9bff61bb75282c3a2a066246b371967a00d4ed
noorfath <fathimanoora5656@gmail.com>
2023-05-17 12:16:40 +0530

    commit

---
+++ bin/maths.dart
@@ -0,0 +1,22 @@
+void add(){
+  int a=10,b=20;
+  print("sum=${a+b}");
+}
+void sub(){
+  int a= 23,b=89;
+  print("difference=${a-b}");
+}
+void div(){
+  int s=40, g=76;
+  print("div=${s/g}");
+}
+void mul(){
+  int a= 43,f=98;
+  print("mul=${a*f}");
+}
+void main(){
+  add();
+  sub();
+  mul();
+  div();
+}
