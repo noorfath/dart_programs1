f0023379b3f8b574f0993efdbb0ae6bc0045a07d
noorfath <fathimanoora5656@gmail.com>
2023-05-17 12:04:02 +0530

    commit

---
+++ bin/localvariables.dart
@@ -0,0 +1,17 @@
+class arithmetic{
+  int a =10,b=20;
+  void add() {
+    int c = 50;
+    print(a + b + c);
+  }
+  void sub(){
+    int z = 57;
+    print(z+a);
+  }
+}
+void main(){
+  arithmetic obj=arithmetic();
+  obj.add();
+  obj.sub();
+
+}
